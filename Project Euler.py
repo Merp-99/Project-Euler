1df4f86c500cbc2e10beba26279dadc86742c722
Merp <prem.pravinkumar@outlook.com>
2025-11-25 08:24:32 +0000

    define sum of multiples

--- Project Euler.py
+++ Project Euler.py
@@ -1,4 +1,7 @@
 # Project Euler Problem 1: Multiples of 3 and 5:
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-
+def sum_of_multiples(k, limit):
+    m = (limit - 1) // k
+    return k * m * (m + 1) // 2
+    
