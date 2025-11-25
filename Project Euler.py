90e76af5c168cf7c3b0f40087802570b01a2e9a5
Merp <prem.pravinkumar@outlook.com>
2025-11-25 08:32:19 +0000

    Solved

--- Project Euler.py
+++ Project Euler.py
@@ -4,4 +4,11 @@
 def sum_of_multiples(k, limit):
     m = (limit - 1) // k
     return k * m * (m + 1) // 2
-    
+
+answer = (
+    sum_of_multiples(3, 1000)
+    + sum_of_multiples(5, 1000) 
+    - sum_of_multiples(15, 1000)
+)
+
+print(answer)
