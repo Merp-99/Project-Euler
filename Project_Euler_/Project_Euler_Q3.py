3116a6ab049f2d123a59f3e8fd6e0fce0cdcbf51
Merp <prem.pravinkumar@outlook.com>
2025-11-25 13:56:22 +0000

    Q3

---
+++ Project_Euler_/Project_Euler_Q3.py
@@ -0,0 +1,18 @@
+# What is the largest prime factor of the number 600851475143?
+
+n = 600851475143
+factor = 2
+largest_factor = 1
+
+while factor * factor <= n:
+    while n % factor == 0:
+        n = n // factor
+        largest_factor = factor
+
+    if factor == 2:
+        factor = 3
+    else:
+        factor += 2
+
+if n > 1:
+    largest_factor = n
