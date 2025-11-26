b889f0c1999b5a6524c3c433bfd304984980c9a0
Merp <prem.pravinkumar@outlook.com>
2025-11-26 07:09:44 +0000

    Q4 solved

---
+++ Project_Euler_/Project_Euler_Q4.py
@@ -0,0 +1,21 @@
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+def is_palindrome(n):
+    s = str(n)
+    rev = s[::-1]
+    return s == rev 
+
+def largest_palindrome_product():
+    largest_palindrome =  0
+    for x in range(100, 1000):
+        for y in range(100, 1000):
+            product = x * y
+            if is_palindrome(product):
+                if product > largest_palindrome:
+                    largest_palindrome = product
+    return largest_palindrome
+
+print(largest_palindrome_product())
+
+
+
